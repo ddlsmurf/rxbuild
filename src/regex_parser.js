eb4a82749d38fee8ebc57e68bb6a1b2784658cb1
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2009-02-16 00:32:51 +0100

    Makefile paths updated

--- src/regex_parser.js
+++ src/regex_parser.js
@@ -24,6 +24,23 @@ var regex_parser = function()
 	this._dbg_withtrace = false;
 	this._dbg_string = new String();
 	
+/* (c) Copyright 2009 Eric Doughty-Papassideris. All Rights Reserved.
+
+	This file is part of RXBuild.
+
+    RXBuild is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RXBuild is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RXBuild.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 	var __regex_parse_result;
 
